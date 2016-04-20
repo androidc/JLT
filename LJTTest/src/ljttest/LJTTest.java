2e73c6e7075581968da6317e4154d40aca0fb038
androidc <chizztectep@gmail.com>
2016-04-20 14:11:32 +0300

    Update LJTTest.java

--- LJTTest/src/ljttest/LJTTest.java
+++ LJTTest/src/ljttest/LJTTest.java
@@ -17,6 +17,7 @@ public class LJTTest {
     public static void main(String[] args) {
         System.out.println("Hello, git");
         System.out.println("1111");
+        System.out.println("hi all");
     }
     
 }
