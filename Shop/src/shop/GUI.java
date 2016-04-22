e3cd24c895322e511d2a2496dde5759ba8df579a
Den <Den>
2016-04-22 16:25:11 +0300

    Delete project.properties

--- Shop/src/shop/GUI.java
+++ Shop/src/shop/GUI.java
@@ -2,6 +2,33 @@ package shop;
 
 // Графический интерфейс
 
-public class GUI {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+
+public class GUI extends JFrame implements ActionListener{
+    
+    // Интерфейс окна сделать в нестандартном виде
+    
+    public GUI(){
+        
+        
+        
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+    }
+    
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent ae){
+        
+    }
+    
+    
     
 }
